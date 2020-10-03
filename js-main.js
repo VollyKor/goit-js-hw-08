5b11cc51e430449eaf9e79a9ff495b7818be81d8
Warness <65484013+Warness@users.noreply.github.com>
2020-10-03 22:32:51 +0300

    Работа над дз
    Выполнена галерея

---
+++ js-main.js
@@ -0,0 +1,58 @@
+import imgArray from './gallery-items.js'
+console.log(imgArray);
+const refs = {
+    galleryList: document.querySelector('.js-gallery'),
+    modal: document.querySelector('div.lightbox'),
+    closeModalBtn: document.querySelector('.lightbox__button'),
+    modalImg: document.querySelector('.lightbox__image')
+}
+
+refs.galleryList.addEventListener('click',onImgClick)
+refs.closeModalBtn.addEventListener('click', onCloseBtnClick)
+
+createGallery(imgArray);
+
+function createGallery (array){
+    const galleryArray = array.map(img => {
+        const listItem = document.createElement('li');
+        const listLink = document.createElement('a');
+        const listImg = document.createElement('img');
+
+        listItem.classList.add('gallery__item');
+        listLink.classList.add('gallery__link');
+        listImg.classList.add('gallery__image');
+        
+        listLink.setAttribute('href', img.original );
+
+        listImg.setAttribute('src' , img.preview);
+        listImg.setAttribute('src' , img.preview);
+        listImg.setAttribute('alt', img.description);
+
+        listImg.dataset.source = img.original;
+
+        listItem.appendChild(listLink);
+        listLink.appendChild(listImg);
+
+        return listItem;
+    })
+    refs.galleryList.append(...galleryArray);
+}
+
+function onCloseBtnClick(){
+    refs.modal.classList.remove('is-open')
+}
+
+function onImgClick (event) {
+    event.preventDefault();
+    const img = event.target
+    if(img.nodeName !== 'IMG'){return};
+
+    refs.modal.classList.add('is-open');
+
+    const fullSizedImageSrc = img.dataset.source;
+    const modalImg = refs.modalImg
+    console.log(fullSizedImageSrc);
+
+    modalImg.src = fullSizedImageSrc;
+
+}
